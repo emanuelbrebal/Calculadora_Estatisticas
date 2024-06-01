ba0767ebea053de82e91252b38dfc383519a0d4b
Emanuel Victor Brebal <manu.brebal@gmail.com>
2024-06-01 09:59:52 -0300

    commit sem a biblioteca statistics

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import statistics
+import math
 
 array_numeros = []
 
 print("""Bem-vindo(a) a calculadora de estatística simples: 
          Informe os valores que serão calculados e a operação logo em seguida.
-         Lembre-se de informar 00 toda vez que quiser parar de informar números.
+         Lembre-se de informar X quando quiser parar de informar números.
       """)
 while (True):
-    num = float(input("""Digite um número: """))
-    if num == 00:
+    num = input("""Digite um número: """)
+    if str(num) == 'X':
         break
     else:
         array_numeros.append(num)
@@ -34,16 +34,49 @@ while(True):
                 soma += array_numeros[i]
             print(f"A média dos valores informados é: {soma/len(array_numeros)}")
         case 2: #mediana
-            mediana = statistics.median(array_numeros)
+            array_numeros.sort()
+            resto = len(array_numeros)%2
+            if resto == 1:
+                posicao = round(len(array_numeros)/2)
+                mediana = array_numeros[posicao]
+            
+            elif resto == 0:
+               posicao = round(len(array_numeros)/2)
+               mediana = (array_numeros[posicao] + array_numeros[posicao-1])/2
+                 
             print(f"A mediana padrão dos valores inseridos é: {mediana}")
 
         case 3: #moda
-            moda = statistics.mode(array_numeros)
-            print(f"A mediana padrão dos valores inseridos é: {moda}")
+            moda_Count = []
+            for i in range(len(array_numeros)):
+                controle = array_numeros[i]
+                moda_M = array_numeros.count(controle)
+                moda_Count.append(moda_M)
+                if moda_M > moda_Count[i]:
+                    maior_C_moda = moda_M
+                    moda = array_numeros[i] 
+                else:
+                    maior_C_moda = moda_Count[i] 
+                    moda = array_numeros[i]
+
+
+            print(f"A moda padrão dos valores inseridos é: {moda}")
         
         case 4: #desvio padrão
-            desvio_padrao = statistics.stdev(array_numeros)
-            print(f"O Desvio padrão dos valores inseridos é: {desvio_padrao}")
+            soma = 0
+            dp_S = 0
+            array_somatorio = []
+            for i in range(len(array_numeros)):
+                soma += float(array_numeros[i])
+            media = soma/len(array_numeros)
+            print(media)
+            for j in range(len(array_numeros)):
+                dp_S += (float(array_numeros[j]) -  media)**2
+                array_somatorio.append(dp_S)
+            print(array_somatorio)
+            Desvio_Padrao = math.sqrt(dp_S)/math.sqrt(len(array_numeros))
+
+            print(f"O Desvio padrão dos valores inseridos é: {Desvio_Padrao}")
 
         case default:
             break
